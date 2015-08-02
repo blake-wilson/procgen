8881a750ba01a5959095760bd89e9016c5ddd567
Blake Wilson <bwilson025@gmail.com>
2015-08-02 17:12:12 -0500

    Add format package and a CSV formatter

---
+++ format/csv_formatter.go
@@ -0,0 +1,21 @@
+package format
+
+import (
+	"fmt"
+
+	"github.com/blake-wilson/procgen/gen"
+)
+
+// Prints each tile as a comma-separated list of attributes beginning with the
+// position. x coord, y coord, attr1, attr2, ...
+func SprintCSV(tiles ...*gen.Tile) string {
+	var str string
+	for _, tile := range tiles {
+		str += fmt.Sprintf("%v, %v", tile.Stop.X, tile.Stop.Y)
+		for _, attr := range tile.Attributes {
+			str += fmt.Sprintf(", %v", attr)
+		}
+		str += "\n"
+	}
+	return str
+}
