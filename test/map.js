0f47a2e49594884fe463de37462f900e9a7396f8
Blake Wilson <bwilson025@gmail.com>
2015-08-03 21:55:09 -0500

    Add simple HTML map tester

---
+++ test/map.js
@@ -0,0 +1,55 @@
+// initialization
+var canvas = document.getElementById("map_canvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.moveTo(0,0);
+
+function drawLine(start, end) {
+	ctx.beginPath();
+	ctx.moveTo(start.x, start.y);
+	ctx.lineTo(end.x, end.y);
+	ctx.strokeStyle = start.color;
+	ctx.lineWidth = 10;
+	console.log(start.color);
+	ctx.stroke();
+}
+
+function getValues(text) {
+	text = document.getElementById("text").value;
+	// assume format x,y\n for each coordinate
+	coordinates = text.split("\n");
+	tileAttrs = [];
+	for (i = 0; i < coordinates.length; i++) {
+		splitAttrs = coordinates[i].split(",");
+		console.log(splitAttrs)
+		if (splitAttrs.length < 3) {
+			continue
+		}
+		var attrs = {
+			x: parseInt(splitAttrs[0]),
+			y: parseInt(splitAttrs[1]),
+			color: '#' + splitAttrs[2].trim(),
+		};
+		tileAttrs.push(attrs);
+	}
+	return tileAttrs
+}
+
+function doIt() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	values = [];
+	values = getValues();
+	for (i = 0; i < values.length - 1; i++) {
+		var fst = {
+			x: values[i].x,
+			y: values[i].y,
+			color: values[i].color,
+		};
+		var snd = {
+			x: values[i + 1].x,
+			y: values[i + 1].y,
+			color: values[i + 1].color,
+		};
+		drawLine(fst, snd);
+	}
+}
